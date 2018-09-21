1f71a6f311c1e6c236a1eb5f00fbee90cb607dec
miguelarteaga <marteagavalle@gmail.com>
2018-09-21 19:31:47 +0200

    ex3

--- ex3.php
+++ ex3.php
@@ -5,3 +5,13 @@ echo "La variable \$d
       conté el valor " . $d->format( "d/m/Y") .
       " i és del tipus $tipus_de_d";
 ?>
+
+<!--
+
+1.- Executen el codi ens surt el següent: "La variable $d conté el valor 21/09/2018 i és del tipus object" .
+
+2.- La variable $d es del tipus object.
+
+4.-  
+
+-->

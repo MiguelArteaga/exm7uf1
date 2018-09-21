be2d09bf9f7b2057c7aa95e63539defdc3e65882
miguelarteaga <marteagavalle@gmail.com>
2018-09-21 17:52:16 +0200

    ex1

--- ex1.php
+++ ex1.php
@@ -10,10 +10,21 @@
 <body>
 
 <?php
-   echo( "<h1>Hello world</H1>" );
+  /* echo( "<h1>Hello world</H1>" );*/
 ?>
+	
 <p>
 Benvinguts al curs de PHP.  
 </p>
 </body>
 </html>
+
+<!--
+1.-La parte que se genera dinamicamente es la parte de PHP "<?php
+  /* echo( "<h1>Hello world</H1>" );*/
+?>"
+
+2.-Desactivan PHP desaparece el titulo h1 de "Hello world"
+
+3.- Al abrir el archivo se nos abre el codigo con un editor de textos.
+-->
